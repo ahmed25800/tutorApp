ac0b4bf4390f009b2e4d4ecb3db32ee2e553e9a9
Ahmed Ashraf <AhmedAshraf52@outlook.com>
2023-06-11 19:17:37 +0300

    Routes

--- routes/chat.js
+++ routes/chat.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/ChattingController");
 const isAuth = require("../middlewares/is_auth");
 router.post("/message/send", isAuth, controller.SendMessage);
-router.delete("/chats/:userId", isAuth, controller.getChatRoomOfUser);
+router.delete("/:userId", isAuth, controller.getChatRoomOfUser);
 router.get("/messages/:chatId", controller.getChatMessages);
 module.exports = router;
